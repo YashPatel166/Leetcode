b76eb521c78b09f8ac3b8d9226e0f32c8bd66250
Yash Patel <60495318+YashPatel166@users.noreply.github.com>
2024-11-07 22:18:21 -0500

    Create Solution.js

---
+++ Strings/Medium/Multiply Strings/Solution.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} num1
+ * @param {string} num2
+ * @return {string}
+ */
+var multiply = function(num1, num2) {
+    let prod = 0;
+    prod = BigInt(num1) * BigInt(num2);
+    return String(prod);
+};
