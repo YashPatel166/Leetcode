1f5000047a76839481131af84a96c08d515643d7
Yash Patel <60495318+YashPatel166@users.noreply.github.com>
2024-11-11 21:53:16 -0500

    Create Solution.js

---
+++ Strings/Medium/Largest Number/Solution.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {string}
+ */
+var largestNumber = function(nums) {
+    let arr = nums.map(String);
+    arr.sort((a, b) => (b + a).localeCompare(a + b));
+    if (arr[0] === "0") {
+        return "0";
+    }
+    return arr.join('');
+};
